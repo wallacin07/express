1354ff5be5f9fff0e01cf2c0983bb307fcf00c36
WallyDev <wallace07abreu@gmail.com>
2025-02-13 15:01:56 -0300

    Vai la paizao

---
+++ backzinho/src/server.ts
@@ -0,0 +1,10 @@
+import express from 'express';
+
+const app = express();
+const port = 8080;
+
+app.listen(port, () => console.log(`Acesse: http://localhost:${port}/`));
+
+app.get('/getTeste', (req, res) => {
+    res.send('GET: Requisição recebida com sucesso!');
+    });
